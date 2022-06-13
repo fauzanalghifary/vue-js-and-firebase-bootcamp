975154ed6be993a59c91db9aa5ddc266d7daaf4c
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-06-13 20:26:53 +0700

    finish section 07

--- Section 06 - Vue Router/ninja-jobs/src/views/jobs/JobDetails.vue
+++ Section 06 - Vue Router/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
+    <div v-if="job">
+      <h1>{{ job.title }}</h1>
+      <p>The job id is {{ id }}</p>
+      <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+      <p>Loading job title ...</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.job = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
   //   data() {
   //     return {
   //       id: this.$route.params.id,
--- Section 06 - Vue Router/ninja-jobs/src/views/jobs/Jobs.vue
+++ Section 06 - Vue Router/ninja-jobs/src/views/jobs/Jobs.vue
@@ -13,12 +13,20 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "Ninja UX Designer", id: 1, details: "lorem" },
-        { title: "Ninja Web Developer", id: 2, details: "lorem" },
-        { title: "Ninja Vue Developer", id: 3, details: "lorem" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.jobs = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
